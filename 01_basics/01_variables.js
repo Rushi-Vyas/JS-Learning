c11b489b0ad6076e43054589432a0bd876a484a3
Rushi Vyas <rushivyas1506@gmail.com>
2026-06-24 06:56:06 +0000

    learned about var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accID = 123
+let accEmail = "abc@gmail.com"
+var accPassward = "1233"
+accCity = "Bhuj"
+let accState
+
+// accID = 23-----> Not Allowed
+accEmail = "fiwjf"
+accPassward = "6787"
+accCity  = "Ahmedabad"
+
+console.table([accID, accEmail, accPassward, accCity, accState])
